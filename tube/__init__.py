151f440cc8affc694603b1a3e8411ec0b60012ad
pchchv <ipchchv@gmail.com>
2023-02-26 04:23:45 +0300

    Init project

---
+++ tube/__init__.py
@@ -0,0 +1,10 @@
+"""
+Tube: a lightweight dependency-free Python library and
+command-line utility for downloading YouTube Videos
+
+"""
+
+__title__ = "tube"
+__author__ = "Evgenii Pochechuev"
+__email__ = "ipchchv@gmail.com"
+__license__ = "Apache-2.0 license"
---
+++ tube/__main__.py
@@ -0,0 +1,11 @@
+"""
+This module implements the basic developer interface for tube.
+The problem domain of the :class:`YouTube <YouTube> class focuses almost
+exclusively on the developer interface. Tube offloads all the hard work
+to smaller peripheral modules and functions.
+
+"""
+
+
+class YouTube:
+    """Core developer interface for pytube."""
