ad20d7121b793ad74d43622470d5f2dd7a82ccb6
pchchv <ipchchv@gmail.com>
2023-02-27 10:47:40 +0300

    Implement a RegexMatchError class

--- tube/exceptions.py
+++ tube/exceptions.py
@@ -12,3 +12,18 @@ class TubeError(Exception):
 
 class ExtractError(TubeError):
     """Data extraction based exception."""
+
+
+class RegexMatchError(ExtractError):
+    """Regex pattern did not return any matches."""
+
+    def __init__(self, caller: str, pattern: Union[str, Pattern]):
+        """
+        :param str caller:
+            Calling function
+        :param str pattern:
+            Pattern that failed to match
+        """
+        super().__init__(f"{caller}: could not find match for {pattern}")
+        self.caller = caller
+        self.pattern = pattern
