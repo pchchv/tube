ec76f3d36e1cee1d94d0461d6df700c574e34d11
pchchv <ipchchv@gmail.com>
2023-02-27 10:55:37 +0300

    Implement a mime_type_codec func

--- tube/extract.py
+++ tube/extract.py
@@ -1,6 +1,7 @@
 """This module contains all non-cipher related data extraction logic."""
-
+import re
 from datetime import datetime
+from typing import Tuple, List
 from tube.helpers import regex_search
 from tube.exceptions import RegexMatchError
 
@@ -21,3 +22,25 @@ def publish_date(watch_html: str):
     except RegexMatchError:
         return None
     return datetime.strptime(result, '%Y-%m-%d')
+
+
+def mime_type_codec(mime_type_codec: str) -> Tuple[str, List[str]]:
+    """Type data parsing.
+    Parses the data in the ``type'' key of the manifest, which contains
+    mime type and codecs serialized together, and parses them into separate
+    elements.
+    **Example**:
+    mime_type_codec('audio/webm; codecs='opus') -> ('audio/webm', ['opus'])
+    :param str mime_type_codec:
+        String containing mime type and codecs.
+    :rtype: tuple
+    :returns:
+        The mime type and list of codecs.
+    """
+    pattern = r"(\w+\/\w+)\;\scodecs=\"([a-zA-Z-0-9.,\s]*)\""
+    regex = re.compile(pattern)
+    results = regex.search(mime_type_codec)
+    if not results:
+        raise RegexMatchError(caller="mime_type_codec", pattern=pattern)
+    mime_type, codecs = results.groups()
+    return mime_type, [c.strip() for c in codecs.split(",")]
