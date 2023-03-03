e16b114b9e5d84ec9aec3e3a8b771498b4ea218f
pchchv <ipchchv@gmail.com>
2023-03-03 07:55:56 +0300

    from_id method of a Youtube class

--- tube/__init__.py
+++ tube/__init__.py
@@ -10,8 +10,8 @@ __author__ = "Evgenii Pochechuev"
 __email__ = "ipchchv@gmail.com"
 __license__ = "Apache-2.0 license"
 
-from tube.version import __version__
 from tube.streams import Stream
 from tube.captions import Caption
-from tube.query import CaptionQuery, StreamQuery
 from tube.__main__ import YouTube
+from tube.version import __version__
+from tube.query import CaptionQuery, StreamQuery
