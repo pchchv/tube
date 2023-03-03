c8048a183325d96edd60f3a2511dd42f16a60eb6
pchchv <ipchchv@gmail.com>
2023-03-03 06:48:08 +0300

    Add version and imports

--- tube/__init__.py
+++ tube/__init__.py
@@ -1,3 +1,4 @@
+# flake8: noqa: F401
 """
 Tube: a lightweight dependency-free Python library and
 command-line utility for downloading YouTube Videos
@@ -8,3 +9,9 @@ __title__ = "tube"
 __author__ = "Evgenii Pochechuev"
 __email__ = "ipchchv@gmail.com"
 __license__ = "Apache-2.0 license"
+
+from tube.version import __version__
+from tube.streams import Stream
+from tube.captions import Caption
+from tube.query import CaptionQuery, StreamQuery
+from tube.__main__ import YouTube
