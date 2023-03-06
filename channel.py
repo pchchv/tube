8c0b5290327086fb9049de6e66c64d8bec4c048b
pchchv <ipchchv@gmail.com>
2023-03-06 23:55:28 +0300

    Implement a html... methods of a Channel class

--- channel.py
+++ channel.py
@@ -1,6 +1,6 @@
 """Module for interacting with a user's youtube channel."""
-from tube import extract
 from typing import Optional, Dict
+from tube import extract, request
 from tube.playlist import Playlist
 
 
@@ -55,3 +55,62 @@ class Channel(Playlist):
         """
         return self.initial_data['metadata'][
             'channelMetadataRenderer'].get('vanityChannelUrl', None)
+
+    @property
+    def html(self):
+        """Get the html for the /videos page.
+        :rtype: str
+        """
+        if self._html:
+            return self._html
+        self._html = request.get(self.videos_url)
+        return self._html
+
+    @property
+    def playlists_html(self):
+        """Get the html for the /playlists page.
+        Currently unused for any functionality.
+        :rtype: str
+        """
+        if self._playlists_html:
+            return self._playlists_html
+        else:
+            self._playlists_html = request.get(self.playlists_url)
+            return self._playlists_html
+
+    @property
+    def community_html(self):
+        """Get the html for the /community page.
+        Currently unused for any functionality.
+        :rtype: str
+        """
+        if self._community_html:
+            return self._community_html
+        else:
+            self._community_html = request.get(self.community_url)
+            return self._community_html
+
+    @property
+    def featured_channels_html(self):
+        """Get the html for the /channels page.
+        Currently unused for any functionality.
+        :rtype: str
+        """
+        if self._featured_channels_html:
+            return self._featured_channels_html
+        else:
+            self._featured_channels_html = request.get(
+                self.featured_channels_url)
+            return self._featured_channels_html
+
+    @property
+    def about_html(self):
+        """Get the html for the /about page.
+        Currently unused for any functionality.
+        :rtype: str
+        """
+        if self._about_html:
+            return self._about_html
+        else:
+            self._about_html = request.get(self.about_url)
+            return self._about_html
