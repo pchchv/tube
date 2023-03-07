5cff1af8bcef6bf5e9be1bbdb47fe30ebd2855f9
pchchv <ipchchv@gmail.com>
2023-03-07 10:14:26 +0300

    Update

--- tube/__init__.py
+++ tube/__init__.py
@@ -9,9 +9,14 @@ __title__ = "tube"
 __author__ = "Evgenii Pochechuev"
 __email__ = "ipchchv@gmail.com"
 __license__ = "Apache-2.0 license"
+__js__ = None
+__js_url__ = None
 
+from tube.search import Search
 from tube.streams import Stream
-from tube.captions import Caption
+from tube.channel import Channel
 from tube.__main__ import YouTube
+from tube.captions import Caption
+from tube.playlist import Playlist
 from tube.version import __version__
 from tube.query import CaptionQuery, StreamQuery
