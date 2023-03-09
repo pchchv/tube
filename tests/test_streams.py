35856877e168113d225b546333b981d77ecf671c
pchchv <ipchchv@gmail.com>
2023-03-09 13:48:37 +0300

    Implement a filesize tests

--- tests/test_streams.py
+++ tests/test_streams.py
@@ -19,3 +19,27 @@ def test_stream_to_buffer(mock_request, cipher_signature):
     cipher_signature.streams[0].stream_to_buffer(buffer)
     # Then
     assert buffer.write.call_count == 3
+
+
+def test_filesize(cipher_signature):
+    assert cipher_signature.streams[0].filesize == 3399554
+
+
+def test_filesize_kb(cipher_signature):
+    assert cipher_signature.streams[0].filesize_kb == float(3319.877)
+
+
+def test_filesize_mb(cipher_signature):
+    assert cipher_signature.streams[0].filesize_mb == float(3.243)
+
+
+def test_filesize_gb(cipher_signature):
+    assert cipher_signature.streams[0].filesize_gb == float(0.004)
+
+
+def test_filesize_approx(cipher_signature):
+    stream = cipher_signature.streams[0]
+
+    assert stream.filesize_approx == 3403320
+    stream.bitrate = None
+    assert stream.filesize_approx == 3399554
